9c01112e75421eba72c1752824d7235ae3cd97e1
ForeverSc <fengshuochn@126.com>
2016-02-29 21:59:13 +0800

    Add get event...

--- 浏览器通用事件处理.js
+++ 浏览器通用事件处理.js
@@ -17,5 +17,26 @@ var EventUtil = {
         } else {
             element["on" + type] = null;
         }
+    },
+    getEvent:function(event){
+        return event?event:window.event;
+    },
+    getTarget: function(event){
+        return event.target||event.srcElement;
+    },
+    preventDefault: function(event){
+        if(event.preventDefault){
+            event.preventDefault();
+        }else{
+            event.returnValue=false;
+        }
+    },
+    stopPropagation: function(event){
+        if(event.stopPropagation){
+            event.stopPropagation();
+        }else{
+            event.cancelBubble=true;
+        }
     }
+    
 };
