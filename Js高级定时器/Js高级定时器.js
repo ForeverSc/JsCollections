594c696382a262e235663cadc31174476d5d6b0b
ForeverSc <fengshuochn@126.com>
2016-07-02 11:30:21 +0800

    add js check fileSize

--- Js高级定时器/Js高级定时器.js
+++
@@ -1,7 +0,0 @@
-function anotherInterval(fn,interval){
-    setTimeout(function(){
-      fn();//这样可以保证每次执行都在函数执行完毕后，若没有执行完新的事件不会加入进程队列中。
-      setTimeout(arguments.callee,interval);
-    },interval);
-}
-
