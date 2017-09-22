cda92828de6565345b4a7cac1068867c146b5bf3
ForeverSc <fengshuochn@126.com>
2017-09-23 01:03:37 +0800

    feature: encode.js

---
+++ JS/jsEncode/encode.js
@@ -0,0 +1,42 @@
+/**
+ * 遍历encode所有值
+ */
+export default function encode(data) {
+    const identify = Object.prototype.toString.call(data)
+    let result
+
+    if (identify === '[object Array]') {
+        result = []
+        Object.keys(data).forEach(key => {
+            result.push(encode(data[key]))
+        })
+    } else if (identify === '[object Object]') {
+        result = {}
+        Object.keys(data).forEach(key => {
+            result[key] = encode(data[key])
+        })
+    } else {
+        result = encodeURIComponent(data)
+    }
+
+    return result
+}
+
+let json = {
+    name: '12313123&123312',
+    obj: {
+        c: [{
+            a: '12313&cc'
+        }]
+    },
+    address: [{
+        a: '123&cc',
+        b: 'c&&c'
+    }, {
+        a: '123&cc',
+        b: 'c&&c',
+    }]
+}
+
+console.log(encode(json))
+
